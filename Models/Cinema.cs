fb2b0152daed08803c680018443850fb8a9d0af9
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-05-30 11:33:17 +0100

    Add Cinema and producer models

---
+++ Models/Cinema.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EticketsWebApp.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int CinemaId { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EticketsWebApp.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ProducerId { get; set; }
+
+        public string profilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
