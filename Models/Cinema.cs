275ebd4d2ecf69ac9bc74ddf31761be227bdc1b1
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-01 11:45:47 +0100

    Add cinemas view

--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -6,8 +6,15 @@ namespace EticketsWebApp.Models
     {
         [Key]
         public int CinemaId { get; set; }
+        [Display(Name ="Cinema logo")]
         public string Logo { get; set; }
+
+        [Display(Name = "Cinema Name")]
+
         public string Name { get; set; }
+
+        [Display(Name = "Cinema Description")]
+
         public string Description { get; set; }
 
         //RelationShips
