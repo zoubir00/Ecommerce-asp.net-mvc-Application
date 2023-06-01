419b4ec8f25397a1134f4a39a432885c7b537a0d
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-01 09:27:33 +0100

    Add producers controller and the Index Action

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using EticketsWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EticketsWebApp.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var producers = await _context.Producers.ToListAsync();
+            return View(producers);
+        }
+    }
+}
