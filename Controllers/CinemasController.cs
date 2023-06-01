246fd92f4714343f9cc5e561a6d5125d9001f01b
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-01 09:33:55 +0100

    Add cinemas controller

---
+++ Controllers/CinemasController.cs
@@ -0,0 +1,21 @@
+﻿using EticketsWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EticketsWebApp.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await _context.Cinemas.ToListAsync();
+            return View(cinemas);
+        }
+    }
+}
