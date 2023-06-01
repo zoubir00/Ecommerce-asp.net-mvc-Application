5d3f8dabc0defd0b8a6ceb36c7db46d0e4bcb6d8
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-01 15:30:23 +0100

    Add IActorServices interface

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,14 @@
+﻿using EticketsWebApp.Models;
+
+namespace EticketsWebApp.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor Newactor);
+        void Delete(Actor actor);
+    }
+}
