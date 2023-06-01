99b122c7ccb9dcfb56ea82af7382af6ab5d12978
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-01 09:39:02 +0100

    Add Movies controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using EticketsWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EticketsWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+    }
+}
