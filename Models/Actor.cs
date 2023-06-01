06c2157a326630ec40a340bb7a425288e679af31
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-01 10:53:55 +0100

    Add actors view

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -7,8 +7,13 @@ namespace EticketsWebApp.Models
         [Key]
         public int ActorId { get; set; }
 
+        [Display(Name ="Profile picture")]
         public string profilePictureURL { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+
+        [Display(Name = "BIO")]
         public string Bio { get; set; }
 
         //Relationships
