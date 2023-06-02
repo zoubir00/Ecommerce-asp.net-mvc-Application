1c2388b33ee5138a68d22b4224c6dfcc5925ffd9
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-02 12:59:50 +0100

    Add base files

---
+++ Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using EticketsWebApp.Models;
+
+namespace EticketsWebApp.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(int id);
+        void Add(T Entity);
+        Task Updateasync(int id, T NewEntity);
+        Task Delete(int id);
+    }
+}
