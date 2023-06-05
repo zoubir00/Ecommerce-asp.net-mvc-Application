cb93d801ab067f4ffefecd3ddecda196bce6c87b
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-05 12:33:02 +0100

    Add some data annotations

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -9,12 +9,16 @@ namespace EticketsWebApp.Models
         public int Id { get ; set ; }
 
         [Display(Name = "Profile picture")]
+        [Required(ErrorMessage ="The image is required")]
         public string profilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full name is required")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="the digits are not enough")]
         public string FullName { get; set; }
 
         [Display(Name = "BIO")]
+        [Required(ErrorMessage = "The biography is required")]
         public string Bio { get; set; }
 
         //rlationShips
