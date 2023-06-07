bd655e99499f1013198b2fe0dc0e0e62985ccf53
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-07 10:43:51 +0100

    Add Orders controller file and get the cart's items

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using EticketsWebApp.Data.Cart;
+using EticketsWebApp.Data.Services;
+using EticketsWebApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EticketsWebApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMovieService _movieService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMovieService movieService, ShoppingCart shoppingCart)
+        {
+            _movieService = movieService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()
+        {
+            var Items = _shoppingCart.GetShoppingCartItem();
+            _shoppingCart.ShoppingCarteItems = Items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                shoppingcartTotal = _shoppingCart.GetShoppingCartTotal()
+            }; 
+            return View();
+        }
+    }
+}
---
+++ Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using EticketsWebApp.Data.Cart;
+
+namespace EticketsWebApp.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart  { get; set; }
+        public double shoppingcartTotal { get; set; }
+    }
+}
