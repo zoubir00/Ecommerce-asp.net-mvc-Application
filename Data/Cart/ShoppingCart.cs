bf74acc7d68b915eb33cf6d676d43426c69fe740
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-07 09:57:46 +0100

    Add shoppingcart class

---
+++ Data/Cart/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using EticketsWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EticketsWebApp.Data.Cart
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCarteItem> ShoppingCarteItems { get; set; }
+
+        public List<ShoppingCarteItem> GetShoppingCartItem()
+        {
+            return ShoppingCarteItems ?? (ShoppingCarteItems = _context.ShoppingCarteItems
+                .Where(n => n.ShoppingCarteId == ShoppingCartId)
+                .Include(m => m.Movie).ToList());
+        }
+        public double GetShoppingCartTotal() => _context.ShoppingCarteItems.Where(n => n.ShoppingCarteId == ShoppingCartId)
+                                                                .Select(n => n.Movie.Price * n.Amount).Sum();
+        
+    }
+}
