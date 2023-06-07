1ee451cdbb70288181d1eea9131bc3404ec40dab
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-07 14:49:22 +0100

    Add the addItemToshoppingcart action

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -27,5 +27,16 @@ namespace EticketsWebApp.Controllers
             }; 
             return View(response);
         }
+
+        public async Task<RedirectToActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _movieService.GetMovieBYIdAsync(id);
+
+            if(item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
