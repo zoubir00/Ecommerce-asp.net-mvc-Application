b32ba5ec1d2a872e1802ced8e8811e30a019dc26
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-08 15:53:06 +0100

    Add identity to the app

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using EticketsWebApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EticketsWebApp.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext:IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
