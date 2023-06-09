41e03ff4af06c9ce0211b0e63023da03c091a2f7
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-09 09:25:46 +0100

    Add login form

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using EticketsWebApp.Data;
+using EticketsWebApp.Data.ViewModels;
+using EticketsWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EticketsWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View(new LoginVM());
+        }
+    }
+}
