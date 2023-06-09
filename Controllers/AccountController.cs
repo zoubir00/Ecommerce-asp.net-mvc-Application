1efeebe42f3f51f6e2aefe389ed1088f9ba7b53c
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-09 11:22:59 +0100

    Logging in users

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -21,9 +21,31 @@ namespace EticketsWebApp.Controllers
         }
 
 
-        public IActionResult Login()
+        public IActionResult Login()=> View(new LoginVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            return View(new LoginVM());
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user =await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result =await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+                ViewData["Error"] = "Wrong credentials, please try again";
+                return View(loginVM);
+            }
+            ViewData["Error"] = "Wrong credentials, please try again";
+            return View(loginVM);
         }
+       
     }
 }
